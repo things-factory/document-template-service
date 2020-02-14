cf5f96c0470498a646a1c5ca2fbae3428bf0aab9
Murky <moosung.gil@hatiolab.com>
2020-02-14 11:36:03 +0900

    change test package name

--- src/test/java/com/hatiolab/things_factory/document_template_service/DocumentTemplateApplicationTests.java
+++ src/test/java/com/hatiolab/things_factory/document_template_service/DocumentTemplateApplicationTests.java
@@ -1,4 +1,4 @@
-package com.hatiolab.thingsfactory.documenttemplate;
+package com.hatiolab.things_factory.document_template_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
